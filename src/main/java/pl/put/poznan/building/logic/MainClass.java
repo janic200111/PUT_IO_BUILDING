12e50f01ccb0b383396a0a12298a4ef3bc5d39bc
acf380 <acf38000@gmail.com>
2022-12-20 12:17:57 +0100

    dodany przykład rest api z sringa

---
+++ src/main/java/pl/put/poznan/building/logic/MainClass.java
@@ -0,0 +1,11 @@
+package pl.put.poznan.building.logic;
+import pl.put.poznan.building.logic.rest.*;
+
+import org.springframework.boot.SpringApplication;
+
+public class MainClass {
+    public static void main(String[] args)
+    {
+        SpringApplication.run(Application.class, args);
+    }
+}
---
+++ src/main/java/pl/put/poznan/building/logic/rest/Controller.java
@@ -0,0 +1,15 @@
+package pl.put.poznan.building.logic.rest;
+
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Controller {
+
+	@GetMapping("/")
+	public String index() {
+		return "Greetings from Spring Boot!";
+	}
+
+}
